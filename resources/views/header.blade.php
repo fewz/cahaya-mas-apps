76418fb30842476660e0e3b3aefe2800bf1ecb8b
William <williamsurya48@gmail.com>
2023-12-19 21:43:27 +0700

    remove unused

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,21 +13,15 @@
     <!-- Font Awesome -->
     <link href="{{ URL::asset('assets/plugins/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
     <!-- icheck bootstrap -->
-    <link href="{{ URL::asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}" rel="stylesheet">
     <!-- Theme style -->
     <link href="{{ URL::asset('assets/dist/css/adminlte.min.css') }}" rel="stylesheet">
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{ URL::asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
     <!-- summernote -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
 
     <!-- jQuery -->
     <script src="{{ URL::asset('assets/plugins/jquery/jquery.min.js') }}"></script>
@@ -46,7 +40,6 @@
     <!-- ChartJS -->
     <script src="{{ URL::asset('assets/plugins/chart.js/Chart.min.js')}}"></script>
     <!-- Sparkline -->
-    <script src="{{ URL::asset('assets/plugins/sparklines/sparkline.js')}}"></script>
     <!-- jQuery Knob Chart -->
     <script src="{{ URL::asset('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
     <!-- daterangepicker -->
@@ -55,28 +48,13 @@
     <!-- Tempusdominus Bootstrap 4 -->
     <script src="{{ URL::asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
     <!-- Summernote -->
-    <script src="{{ URL::asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
     <!-- overlayScrollbars -->
     <script src="{{ URL::asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 
     <!-- DataTables -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
 
     <!-- DataTables  & Plugins -->
     <script src="{{ URL::asset('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/jszip/jszip.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/pdfmake/pdfmake.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/pdfmake/vfs_fonts.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{ URL::asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
 
     <style>
         input[type='file'].is-invalid {
